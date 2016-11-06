f565dac2b26c8bad5667048533f91ccde2490b9e
Phan Y Biển <phanybien@gmail.com>
2016-11-06 16:12:06 +0700

    * Fix conflict

--- GUI/src/app/app.component.ts
+++ GUI/src/app/app.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
-import { Platform, AlertController } from "ionic-angular";
+import { Component ,ViewChild} from '@angular/core';
+import { Nav, Platform, AlertController } from "ionic-angular";
 import { StatusBar, Splashscreen } from "ionic-native";
 import { TranslateService, TranslateLoader, TranslateStaticLoader } from "ng2-translate";
 import { Http } from "@angular/http";
+import { LoginViewModel } from '../viewmodels/user.login.viewmodel';
+import { EditUserViewModel } from '../viewmodels/user.edituser.viewmodel';
+import { ViewListUserViewModel } from '../viewmodels/user.viewlistuser.viewmodel';
+import { HomeViewModel } from '../viewmodels/home.viewmodel';
 import './rxjs-operators';
 // View Models
 import { Constants } from '../constants/constants';
@@ -10,6 +14,7 @@ import { SignUpViewModel } from '../viewmodels/user.signup.viewmodel';
 
 @Component({
 	template: `<ion-nav [root]="rootPage"></ion-nav>`,
+	templateUrl: '../pages/menu/menu.html',
 	providers: [
 		{
 			provide: TranslateLoader,
@@ -20,7 +25,9 @@ import { SignUpViewModel } from '../viewmodels/user.signup.viewmodel';
 	]
 })
 export class WAW_Application {
+	@ViewChild(Nav) nav: Nav;
 	rootPage = SignUpViewModel;
+	pages: Array<{title: string, icon: string, component: any}>;
 
 	constructor(private alertCtrl: AlertController, private platform: Platform, private translate: TranslateService) {
 		platform.ready().then(() => {
@@ -33,5 +40,15 @@ export class WAW_Application {
 			translate.setDefaultLang(Constants.LANGUAGE_EN);
 			translate.use(Constants.LANGUAGE_EN);
 		});
+		// used for an example of ngFor and navigation
+		this.pages = [
+			{ title: 'Edit Profile', icon: 'person', component: EditUserViewModel },
+			{ title: 'View List User', icon: 'search', component: ViewListUserViewModel }
+		];
 	}
+	openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
 }
